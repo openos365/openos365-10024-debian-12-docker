db8a2c65fbfe70f183821cee7d83d17f6a04fc0e
gnuhub <gnuhub@qq.com>
2024-01-08 23:05:12 +0800

    [just fix it]

--- ci/1010-shell-run/1.ci.run.sh
+++ ci/1010-shell-run/1.ci.run.sh
@@ -9,5 +9,22 @@ cd $CMD_PATH
 echo "============================================================================"
 # TODO HERE
 
+date
+
+distro=bookworm
+mirror=https://mirrors.tuna.tsinghua.edu.cn/debian/
+
+rootfsDir=chroot
+debootstrap=debootstrap
+
+if [  -d $rootfsDir ];then
+    rm -rf $rootfsDir
+fi
+
+debootstrap \
+--arch=amd64 \
+--variant=minbase \
+--include=zsh \
+$distro $rootfsDir $mirror
 
 echo "============================================================================"
