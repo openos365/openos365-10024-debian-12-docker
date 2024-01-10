a2baddfb8fa72ee9782004efb5fc71797c913063
gnuhub <gnuhub@qq.com>
2024-01-10 10:44:23 +0800

    [just fix it]

--- ci/1024-docker-build-xfce-v2/files/install.sh
+++ ci/1024-docker-build-xfce-v2/files/install.sh
@@ -23,6 +23,21 @@ rsync -avzP ./mychroot/etc/ /etc/
 apt update -y
 apt upgrade -y
 
+sudo df -h
+
+# https://github.com/jlumbroso/free-disk-space/blob/main/action.yml
+sudo rm -rf /opt/ghc
+
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
+sudo apt-get autoremove -y
+sudo apt-get clean
+echo "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 
 
 function nvm_install()
