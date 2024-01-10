456d37f1e158557cdad4bd14eb4d04cf5d1f81ba
gnuhub <gnuhub@qq.com>
2024-01-10 08:45:41 +0800

    build xfce v2

---
+++ ci/1024-docker-build-xfce-v2/files/install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+echo "============================================================================"
+pwd
+
+echo "============================================================================"
+whoami
+
+echo "============================================================================"
+env
+
+echo "============================================================================"
+# TODO HERE
+cd $CMD_PATH
+ls -al 
+rsync -avzP ./mychroot/etc/ /etc/
+apt update -y
+apt upgrade -y
+
+
+
+function nvm_install()
+{
+cd ~
+git clone https://github.com/nvm-sh/nvm.git .nvm
+cd .nvm
+git checkout v0.39.5
+
+cd ~
+. ~/.nvm/nvm.sh
+
+for node_version in "v14.21.3"
+do
+  nvm install $node_version
+  nvm use $node_version
+  npm install yarn -g
+  npm install pnpm -g
+  yarn global add prettier
+done
+
+nvm use v14.21.3
+npm install meteor -g --unsafe-perm
+
+export PATH=$HOME/.meteor:$PATH
+echo $PATH
+which node
+node --version
+
+rsync -avzP /root/.nvm/ /etc/skel/.nvm/
+}
+
+nvm_install
+
+
+
+echo "============================================================================"
