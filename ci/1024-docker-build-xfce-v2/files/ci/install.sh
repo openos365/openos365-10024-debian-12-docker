afaa2c40f4d960c2a1c3d0311f81140dc05f5e2a
gnuhub <gnuhub@gmail.com>
2024-02-17 07:32:12 +0800

    Update install.sh

--- ci/1024-docker-build-xfce-v2/files/ci/install.sh
+++ ci/1024-docker-build-xfce-v2/files/ci/install.sh
@@ -55,6 +55,36 @@ rsync -avzP /root/.nvm/ /etc/skel/.nvm/
 
 nvm_install
 
+apt install caddy -y
+
+apt autoremove -y
+
+apt remove --purge -y $(dpkg -l | grep linux-image | awk '{print $2}')
+dpkg -l | grep linux-image | awk '{print $2}'
+apt update -y
+apt upgrade -y
+apt -y install --no-install-recommends linux-image-amd64
+
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+mkdir -p /etc/sudoers.d
+echo "www ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/www-nopassword
+echo "runner ALL=(ALL) NOPASSWD: ALL"   > /etc/sudoers.d/runner-nopassword
+chmod 750 /etc/sudoers.d/www-nopassword
+chmod 750 /etc/sudoers.d/runner-nopassword
+chmod 750 /etc/sudoers.d/
+cat /etc/passwd
+
+apt install build-essential libjpeg-dev libpng-dev libtiff-dev -y
+apt install python3 -y
+apt install python3-pip -y
+apt install python3-selenium -y
+pip install selenium-wire --break-system-packages
+
+apt install chromium-driver  -y
+apt install chromium  -y
+
 
 apt update -y
 apt upgrade -y
