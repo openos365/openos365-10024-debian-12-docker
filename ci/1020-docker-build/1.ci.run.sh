dfeecd20bc27b878ab93ce3b14217b30a86ade02
gnuhub <gnuhub@qq.com>
2024-02-20 01:00:31 +0800

    [just fix it]

--- ci/1020-docker-build/1.ci.run.sh
+++ ci/1020-docker-build/1.ci.run.sh
@@ -4,28 +4,53 @@ set -x
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
-export MY_USER=root
+cd ~
+wget https://mirrors.ustc.edu.cn/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.4_all.deb
+sudo dpkg -i debian-archive-keyring_2023.4_all.deb
+cd $CMD_PATH
+
+date
+sudo apt install debootstrap -y
+distro=bookworm
+mirror=http://mirrors.ustc.edu.cn/debian/
+
+rootfsDir=chroot
+debootstrap=debootstrap
+
+if [  -d $rootfsDir ];then
+    rm -rf $rootfsDir
+fi
+
+sudo debootstrap \
+--arch=amd64 \
+--include=zsh,locales,locales-all,rsync \
+--components=main,contrib,non-free,non-free-firmware \
+--variant=minbase \
+$distro $rootfsDir $mirror
+
+sudo tar -C chroot -cf ./rootfs.tar .
+
 # echo "openos365" > files/root/etc/hostname
 
 docker build . -f Dockerfile \
 --progress plain \
--t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER \
--t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest \
--t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER \
--t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest \
--t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER \
--t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest \
--t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER \
--t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
 
 
-# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER
-# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest
-docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER
-docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest
-# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER 
-# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest 
-# docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:$GITHUB_RUN_NUMBER
-# docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME-${MY_USER}:latest
+# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER 
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest 
+docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
 
 echo "============================================================================"
