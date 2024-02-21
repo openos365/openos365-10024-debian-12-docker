b5261cce74ea424101d700ad58fbf9a68d32041f
gnuhub <gnuhub@qq.com>
2024-02-21 20:21:04 +0800

    [just fix it]

--- ci/100-v0/files/ci/install.sh
+++ ci/100-v0/files/ci/install.sh
@@ -21,6 +21,12 @@ rm -rf \
         /var/tmp/* \
         /var/log/* \
         /root/.cache
-df -h
 
+mkdir -p /etc/buildinfo/v0/
+cd /etc/buildinfo/v0/
+
+date > /build_date.txt
+apt list --installed > apt.list.installed.txt
+apt list > /apt.list.txt
+df -h > df.txt
 
