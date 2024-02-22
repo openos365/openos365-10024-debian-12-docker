1efecf0a1cdf1bbea9bbd5e8bc7c277925669f5b
gnuhub <gnuhub@qq.com>
2024-02-22 08:56:57 +0800

    [just fix it]

--- ci/v23q3/files/ci/install.sh
+++ ci/v23q3/files/ci/install.sh
@@ -7,38 +7,6 @@ echo $PROJECT_NAME
 cd $CMD_PATH
 export DEBIAN_FRONTEND="noninteractive"
 
-
-# TODO HERE
 cd $CMD_PATH
-
-function my_debconf_set_selections()
-{
-	cd $CMD_PATH
-	while read conf
-	do
-  		echo $conf_str
-  		debconf-set-selections <<< "$conf_str"
-	done < debconf.txt
-}
-
-function my_pkg_install_remove()
-{
-	cd $CMD_PATH
-	while read pkg
-	do
-  		echo $pkg
-  		apt install -y --no-install-recommends $pkg
-	done < packages.list.txt
-
-	while read pkg
-	do
- 		echo $pkg
-  		apt install -y $pkg
-	done < remove.list.txt
-}
-
-update_clean
-my_debconf_set_selections
-my_pkg_install_remove
-update_clean v23q3
+update_install_remove_clean v23q1 remove
 
