e8c53c1f6afbc3a82bc023c644f352d069e3129c
gnuhub <gnuhub@qq.com>
2024-02-22 08:25:48 +0800

    add update_clean v23q1

--- ci/v23q1/files/ci/install.sh
+++ ci/v23q1/files/ci/install.sh
@@ -6,27 +6,6 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 export TERM=xterm-256color
 echo $PROJECT_NAME
 cd $CMD_PATH
-env
-whoami
-pwd
-apt update -y
-apt upgrade -y
-apt autoremove -y
-apt clean
-apt-get autoremove -y 
-apt-get clean 
-rm -rf \
-        /tmp/* \
-        /var/lib/apt/lists/* \
-        /var/tmp/* \
-        /var/log/* \
-        /root/.cache
 
-mkdir -p /etc/buildinfo/v0/
-cd /etc/buildinfo/v0/
-
-date > /build_date.txt
-apt list --installed > apt.list.installed.txt
-apt list > /apt.list.txt
-df -h > df.txt
+update_clean v23q1
 
