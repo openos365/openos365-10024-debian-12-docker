4b9402eb89f1994f66ba7ed33bb784be8ccf9a86
gnuhub <gnuhub@qq.com>
2024-02-22 14:39:58 +0800

    [just fix it]

--- ci/v24q1/files/ci/install.sh
+++ ci/v24q1/files/ci/install.sh
@@ -6,30 +6,4 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 echo $PROJECT_NAME
 cd $CMD_PATH
 
-echo "============================================================================"
-pwd
-
-echo "============================================================================"
-whoami
-
-echo "============================================================================"
-env
-
-echo "============================================================================"
-# TODO HERE
-cd $CMD_PATH
-ls -al
-apt update -y
-apt upgrade -y
-
-
-mkdir -p /etc/buildinfo/v24q1/
-cd /etc/buildinfo/v24q1/
-
-date > /build_date.txt
-apt list --installed > apt.list.installed.txt
-apt list > /apt.list.txt
-df -h > df.txt
-
-
-echo "============================================================================"
+update_install_remove_clean v24q1 remove
