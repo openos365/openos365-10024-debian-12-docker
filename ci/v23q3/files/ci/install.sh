b8ad062532bd35047ab863a9f2dde066d1119daf
gnuhub <gnuhub@qq.com>
2024-02-22 08:57:42 +0800

    [just fix it]

---
+++ ci/v23q3/files/ci/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+export DEBIAN_FRONTEND="noninteractive"
+
+cd $CMD_PATH
+update_install_remove_clean v23q2 remove
+
