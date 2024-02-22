99da710882eb6354f4d68bf68cc7ef532448aae3
gnuhub <gnuhub@qq.com>
2024-02-22 23:30:36 +0800

    [just fix it]

---
+++ ci/v23q4/files/ci/conf/scripts/101.ci.script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export TERM=xterm-256color
+cd $CMD_PATH
+date
+pip install selenium-wire --break-system-packages
+pip install scrapeops-scrapy --break-system-packages
+pip install scrapeops-scrapy-proxy-sdk --break-system-packages
