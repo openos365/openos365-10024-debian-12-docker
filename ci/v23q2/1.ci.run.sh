7b4100854d6ad64a048916a9087b1c01461203d8
gnuhub <gnuhub@qq.com>
2024-02-23 20:05:38 +0800

    fix docker cp

---
+++ ci/v23q2/1.ci.run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+cd $CMD_PATH
+
+
+
+docker build . -f Dockerfile \
+--progress plain \
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
+-t registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+
+
+# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+# docker push ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+ docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+ docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER 
+# docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest 
+docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
+docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
+
+cid=`docker run -d ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest /bin/true`
+docker cp $cid:/etc/buildinfo/ "${CMD_PATH}/"
+
