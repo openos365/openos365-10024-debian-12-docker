8b82f55d8c2d635318e19ffa5dfdb85d8f736186
gnuhub <gnuhub@qq.com>
2024-02-23 20:59:01 +0800

    fix docker cp

--- ci/v23q2/1.ci.run.sh
+++ ci/v23q2/1.ci.run.sh
@@ -27,6 +27,8 @@ docker build . -f Dockerfile \
 docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
 docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
 
+cd $CMD_PATH
+rm -rf buildinfo
 cid=`docker run -d ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest /bin/true`
-docker cp $cid:/etc/buildinfo/ "${CMD_PATH}/ect_buildinfo/"
+docker cp $cid:/etc/buildinfo/ "${CMD_PATH}/"
 
