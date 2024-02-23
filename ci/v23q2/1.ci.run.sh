20436fb3acd945de531d1332267e666cc4bab9e9
gnuhub <gnuhub@qq.com>
2024-02-23 19:16:14 +0800

    get buildinfo

--- ci/v23q2/1.ci.run.sh
+++ ci/v23q2/1.ci.run.sh
@@ -27,4 +27,6 @@ docker build . -f Dockerfile \
 docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
 docker push ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest
 
-echo "============================================================================"
+cid=`docker run -d ${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest /bin/true`
+docker cp $cid:/etc/buildinfo/ "${CMD_PATH}/ect_buildinfo/"
+
