1ee7b520970a42e4e4a04b5e72e576b0d6d7953e
gnuhub <gnuhub@qq.com>
2024-02-25 12:13:00 +0800

    [just fix it]

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -19,60 +19,6 @@ env
 echo "============================================================================"
 if [ ! -z "${GITHUB_REPOSITORY}" ];then
 
-sudo df -h
-sudo rm -rf /usr/local/lib/android # will release about 10 GB if you don't need Android
-sudo rm -rf /usr/share/dotnet # will release about 20GB if you don't need .NET
-
-# sudo docker image prune --all --force
-
-# https://github.com/jlumbroso/free-disk-space/blob/main/action.yml
-sudo rm -rf /opt/ghc
-
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y '^mongodb-.*'
-sudo apt-get remove -y '^mysql-.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
-sudo apt-get autoremove -y
-sudo apt-get clean
-echo "$AGENT_TOOLSDIRECTORY"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
-sudo df -h
-
-    # 0
-    # sudo apt update -y
-    # sudo apt upgrade -y
-    sudo apt install -y dos2unix
-    cd ~
-    curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64
-    sudo install container-diff-linux-amd64 /usr/bin/container-diff
-    cd $CMD_PATH
-    
-    # 1 
-    git config --global user.email "gnuhub@gmail.com"
-    git config --global user.name "gnuhub"
-
-    # 2
-    git remote -v
-    git remote set-url origin git@github.com:${GITHUB_REPOSITORY}.git
-    git remote -v
-
-    # 3
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "frs.sourceforge.net"
-    ssh-keyscan "frs.sourceforge.net" >> $HOME/.ssh/known_hosts
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "github.com"
-    ssh-keyscan "github.com" >> $HOME/.ssh/known_hosts
-    cat $HOME/.ssh/known_hosts
-
-    # 4
-    sudo cp ./p2 /usr/bin/p2
-
-    # 5
-    cd $CMD_PATH
-    p2 "CI-BOT:$(date +%Y.%m.%d-%H%M%S)-$GITHUB_REF_NAME-$GITHUB_RUN_NUMBER"
-
-
     # 6
     cd $CMD_PATH
     for ci_dir in `ls -d -1 ci/v* | sort`
@@ -82,34 +28,6 @@ sudo df -h
             ./${ci_dir}/1.ci.run.sh
         fi
     done
-
-    # 7
-    # cd $CMD_PATH
-    # apt list > 4.apt.list.txt
-    # apt list --installed > 5.apt.list.installed.txt
-   
-    # 8
-    cd $CMD_PATH
-    touch 8.workflows.to.run.repos.txt
-    dos2unix 8.workflows.to.run.repos.txt
-    while read repo
-    do
-        echo $repo | tr -d '\r'
-        export repo_name=$(echo $repo | tr -d '\r')
-        echo $repo_name
-        if [ ! -z $repo_name ];then 
-            export repo_name_length=$(expr length ${repo_name})
-            echo $repo_name_length
-            if [ $repo_name_length -gt 3 ];then
-                gh workflow run 1.ci.yml --repo $repo_name --ref $GITHUB_REF_NAME
-            fi
-        fi
-    done < 8.workflows.to.run.repos.txt
-
-    # 9
-    sudo find ./ -name "*.sh" -exec sudo chmod +x {} \; 
-    p2 "CI-BOT:$(date +%Y.%m.%d-%H%M%S)-$GITHUB_REF_NAME-$GITHUB_RUN_NUMBER"
-    
 
 fi
 echo "============================================================================"
--- ci/v23q4/1.ci.run.sh
+++ ci/v23q4/1.ci.run.sh
@@ -4,39 +4,6 @@ set -x
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
-sudo df -h
-sudo rm -rf /usr/local/lib/android # will release about 10 GB if you don't need Android
-sudo rm -rf /usr/share/dotnet # will release about 20GB if you don't need .NET
-# https://github.com/jlumbroso/free-disk-space/blob/main/action.yml
-sudo rm -rf /opt/ghc
-sudo rm -rf /usr/local/.ghcup || true
-sudo rm -rf ~/.nvm/
-sudo rm -rf ~/.m2/
-rm -rf ~/.nvm/
-rm -rf ~/.m2/
-sudo apt-get remove -y '^aspnetcore-.*' || echo "::warning::The command [sudo apt-get remove -y '^aspnetcore-.*'] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y '^dotnet-.*' --fix-missing || echo "::warning::The command [sudo apt-get remove -y '^dotnet-.*' --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y '^llvm-.*' --fix-missing || echo "::warning::The command [sudo apt-get remove -y '^llvm-.*' --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y 'php.*' --fix-missing || echo "::warning::The command [sudo apt-get remove -y 'php.*' --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y '^mongodb-.*' --fix-missing || echo "::warning::The command [sudo apt-get remove -y '^mongodb-.*' --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y '^mysql-.*' --fix-missing || echo "::warning::The command [sudo apt-get remove -y '^mysql-.*' --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y azure-cli google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri --fix-missing || echo "::warning::The command [sudo apt-get remove -y azure-cli google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y google-cloud-sdk --fix-missing || echo "::debug::The command [sudo apt-get remove -y google-cloud-sdk --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get remove -y google-cloud-cli --fix-missing || echo "::debug::The command [sudo apt-get remove -y google-cloud-cli --fix-missing] failed to complete successfully. Proceeding..."
-sudo apt-get autoremove -y || echo "::warning::The command [sudo apt-get autoremove -y] failed to complete successfully. Proceeding..."
-sudo apt-get clean || echo "::warning::The command [sudo apt-get clean] failed to complete successfully. Proceeding..."
-
-sudo docker image prune --all --force
-
-sudo rm -rf /opt/hostedtoolcache/CodeQL
-
-sudo swapoff -a || true
-sudo rm -f /mnt/swapfile || true
-free -h
-
-echo "$AGENT_TOOLSDIRECTORY"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
-sudo df -h
 
 docker build . -f Dockerfile \
 --progress plain \
