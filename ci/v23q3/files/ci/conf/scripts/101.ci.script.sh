7c194bd9497c7bcf7f4f9cae3ffba8fbe9be5521
gnuhub <gnuhub@qq.com>
2024-02-25 09:45:02 +0800

    install xunlei

--- ci/v23q3/files/ci/conf/scripts/101.ci.script.sh
+++ ci/v23q3/files/ci/conf/scripts/101.ci.script.sh
@@ -5,4 +5,8 @@ export TERM=xterm-256color
 cd $CMD_PATH
 date
 echo "root:act365" | chpasswd
+cd /opt
+wget https://cdn-package-store6.deepin.com/appstore/pool/appstore/c/com.xunlei.download/com.xunlei.download_1.0.0.2_amd64.deb
+apt install -y com.xunlei.download_1.0.0.2_amd64.deb
+rm -rf com.xunlei.download_1.0.0.2_amd64.deb
 date
